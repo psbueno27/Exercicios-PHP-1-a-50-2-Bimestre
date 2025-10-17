d6b71160061dcb74ba194f09644b4aab7ffc5911
LUCAS TEIXEIRA DA SILVA <lucaslobo2019@gmail.com>
2025-10-17 18:28:35 -0300

    Add course information and comments to exercicio33.php
    Added header comments with course and student details.

--- exercicio33.php
+++ exercicio33.php
@@ -1,4 +1,22 @@
 <?php
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                Guilherme Costa Ferreira (RA: 25068539-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Lucas Teixeira da Silva (RA: 25356144-2)
+                Pedro Bueno (RA: 25181992-2)
+                Sadrak Araújo (RA: 25356591-2)      
+    
+Data: 8 de outubro de 2025
+Procedimento: Crie um procedimento mostrarTabuada que receba um número e imprima sua tabuada de 1 a 10.            
+***************************/
 
 function mostrarTabuada($numero) {
     echo "Tabuada do $numero:<br>";
