aec76f48ec1cf0811f5b289629c00e297eb02099
Funesto <xxtbrgamesbrcosta@gmail.com>
2025-10-19 19:14:22 -0300

    Create exercicio48.php

---
+++ exercicio48.php
@@ -0,0 +1,30 @@
+<?php
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                Guilherme Costa Ferreira (RA: 25068539-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Lucas Teixeira da Silva (RA: 25356144-2)
+                Pedro Bueno (RA: 25181992-2)
+                Sadrak Araújo (RA: 25356591-2)      
+    
+Data: 8 de outubro de 2025
+Descritivo: .Função: Escreva uma função fatorial que receba um número e retorne seu fatorial             
+***************************/
+function fatorial($numero) {
+    $resultado = 1;
+
+    for ($i = 1; $i <= $numero; $i++) {
+        $resultado *= $i;
+    }
+    return $resultado;
+}
+
+echo fatorial(5); 
+?>
