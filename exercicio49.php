dc41578eb4196de0ce20d97f016eb2649581f6e8
Funesto <xxtbrgamesbrcosta@gmail.com>
2025-10-19 19:23:27 -0300

    Create exercicio49.php

---
+++ exercicio49.php
@@ -0,0 +1,31 @@
+<?php
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                Guilherme Costa Ferreira (RA: 25068539-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Lucas Teixeira da Silva (RA: 25356144-2)
+                Pedro Bueno (RA: 25181992-2)
+                Sadrak Araújo (RA: 25356591-2)      
+    
+Data: 8 de outubro de 2025
+Descritivo: .Função: Faça uma função gerarArrayPares que receba um número n e retorne um array com todos os números pares de 0 até n.          
+***************************/
+function gerarArrayPares($n) {
+    $pares = [];
+
+    for ($i = 0; $i <= $n; $i++) {
+        if ($i % 2 == 0) {
+            $pares[] = $i;
+        }
+    }
+    return $pares;
+}
+print_r(gerarArrayPares(50));  
+?>
