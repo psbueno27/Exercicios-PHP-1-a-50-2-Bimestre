c03cf19313d7fa6114c61f639a4aaecd230bab0b
Funesto <xxtbrgamesbrcosta@gmail.com>
2025-10-19 19:11:04 -0300

    Create exercicio47.php

---
+++ exercicio47.php
@@ -0,0 +1,35 @@
+<?php
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                Guilherme Costa Ferreira (RA: 25068539-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Lucas Teixeira da Silva (RA: 25356144-2)
+                Pedro Bueno (RA: 25181992-2)
+                Sadrak Araújo (RA: 25356591-2)      
+    
+Data: 8 de outubro de 2025
+Descritivo: .Função com String: Crie uma função contarVogais que receba uma string e retorne o número de vogais..             
+***************************/
+function contarVogais($texto) {
+    $vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    $contador = 0;
+
+    for ($i = 0; $i < strlen($texto); $i++) {
+        if (in_array($texto[$i], $vogais)) {
+            $contador++;
+        }
+    }
+
+    return $contador;
+}
+
+$frase = "Professor";
+echo contarVogais($frase); 
+?>
