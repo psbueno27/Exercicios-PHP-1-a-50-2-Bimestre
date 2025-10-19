1ed187217457a3d89cc3a9357e8d1b0ce39077cf
Funesto <xxtbrgamesbrcosta@gmail.com>
2025-10-19 18:44:49 -0300

    Create exercicio44.php

---
+++ exercicio44.php
@@ -0,0 +1,29 @@
+<?php
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                Guilherme Costa Ferreira (RA: 25068539-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Lucas Teixeira da Silva (RA: 25356144-2)
+                Pedro Bueno (RA: 25181992-2)
+                Sadrak Araújo (RA: 25356591-2)      
+    
+Data: 8 de outubro de 2025
+Descritivo: Escreva um programa que use for para imprimir os números de 1 a 10.             
+***************************/
+$largura=20;
+$altura=10;
+$area=$largura * $altura;
+    echo "A área do retângulo é: ";
+    echo $area;
+
+function calcularAreaRetangulo($largura, $altura) {
+    return $largura * $altura;
+}
+?>
